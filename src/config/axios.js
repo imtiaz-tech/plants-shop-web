da3d8e38b1843a7eb0986c943b412ba7b9fa15ae
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-10-02 14:08:51 +0500

    fixes

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5400/api/v1/"
-    : "https://plants-shop-web-rl6e-nc3ndq7pm-imtiaz-techs-projects.vercel.app/api/v1/";
+    // : "https://plants-shop-web-rl6e-ew5y9t92d-imtiaz-techs-projects.vercel.app/api/v1/";
+    : "https://plants-shop-backend.vercel.app/api/v1/";
 
 export default axios;
