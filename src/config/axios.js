a6e00899264fbc8627b77fbc8ceaf2c3fadcdcb8
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-10-02 11:53:37 +0500

    fix css issue

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5400/api/v1/"
-    : "https://decor-plant-imtiaz-tech-imtiaz-techs-projects.vercel.app/api/v1/";
+    : "https://plants-shop-web-rl6e-nc3ndq7pm-imtiaz-techs-projects.vercel.app/api/v1/";
 
 export default axios;
